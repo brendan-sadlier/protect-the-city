04b047400878dc9c0cec2a82293c6e852adfc827
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-02-20 21:54:05 +0000

    Player 2 Sprite Changes

--- src/Player/PlayerTwo.java
+++ src/Player/PlayerTwo.java
@@ -7,6 +7,6 @@ import util.Vector3f;
     public class PlayerTwo extends Player {
 
     public PlayerTwo () {
-        super("res/sprites/Tank_Blue_Sprite.png",new Point3f(750,590,0));
+        super("res/sprites/Tank_Red_Sprite.png",new Point3f(750,590,0));
     }
 }
